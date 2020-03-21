50ac53c5c38feb115d248baf3ca70e94b4ea6626
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-03-21 14:13:56 +0530

    musicplayer updated

--- App.js
+++ App.js
@@ -1,15 +1,102 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, Button} from 'react-native';
+import TrackPlayer, {
+  TrackPlayerEvents,
+  STATE_PLAYING,
+} from 'react-native-track-player';
+import {
+  useTrackPlayerProgress,
+  useTrackPlayerEvents,
+} from 'react-native-track-player/lib/hooks';
+import Slider from '@react-native-community/slider';
+
+const trackPlayerInit = async () => {
+  await TrackPlayer.setupPlayer();
+  TrackPlayer.updateOptions({
+    stopWithApp: true,
+    capabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_JUMP_FORWARD,
+      TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+    ],
+  });
+  await TrackPlayer.add({
+    id: '1',
+    url:
+      'https://audio-previews.elements.envatousercontent.com/files/103682271/preview.mp3',
+    type: 'default',
+    title: 'My Title',
+    album: 'My Album',
+    artist: 'Rohan Bhatia',
+    artwork: 'https://picsum.photos/100',
+  });
+  return true;
+};
 
 const App = () => {
+  const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const {position, duration} = useTrackPlayerProgress(250);
+
+  useEffect(() => {
+    setIsTrackPlayerInit(trackPlayerInit);
+  }, []);
+
+  useEffect(() => {
+    if (!isSeeking && isPlaying) {
+      setSliderValue(position / duration);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [position, duration]);
+
+  useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], event => {
+    if (event.state === STATE_PLAYING) {
+      setIsPlaying(true);
+    } else {
+      setIsPlaying(false);
+    }
+  });
+
+  const onButtonPressed = () => {
+    if (!isPlaying) {
+      TrackPlayer.play();
+      //setIsPlaying(true);
+    } else {
+      TrackPlayer.pause();
+      //setIsPlaying(false);
+    }
+  };
+
+  const slidingStarted = () => {
+    setIsSeeking(true);
+  };
+
+  const slidingCompleted = async value => {
+    await TrackPlayer.seekTo(value * duration);
+    setSliderValue(value);
+    setIsSeeking(false);
+  };
+
   return (
     <>
       <Text>Music Player</Text>
       <Button
-        title="Play/Pause"
-        onPress={() => {
-          console.log('pressed');
-        }}
+        title={isPlaying ? 'Pause' : 'Play'}
+        onPress={onButtonPressed}
+        disabled={!isTrackPlayerInit}
+      />
+      <Slider
+        style={{width: 400, height: 40}}
+        minimumValue={0}
+        maximumValue={1}
+        value={sliderValue}
+        minimumTrackTintColor="#111000"
+        maximumTrackTintColor="#000000"
+        onSlidingStart={slidingStarted}
+        onSlidingComplete={slidingCompleted}
       />
     </>
   );
